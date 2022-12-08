3200f8b5360dc9f574d65b4549a46d7f10c2015c
martin <91570703+Marvian-programmer@users.noreply.github.com>
2022-12-07 19:52:51 -0700

    added more code for the prime number problem.

--- Algorithm Design - Python/primes/finding_Primes.py
+++ Algorithm Design - Python/primes/finding_Primes.py
@@ -3,18 +3,21 @@ import math
 while True:
     # This Array will a range of numbers
     # based on the user input.
+    numbers = []
     primes = []
-    primes_list = []
 
     # Prompt for user input
     user_input = int(input("Enter a number: "))
+    
 
     # Creating a range of numbers based on 
     # user input and then inserting them in a list.
     for number in range(1, user_input):
+            
         number+=1
-        primes.append(number)
-    
+        assert type(numbers) == list
+        numbers.append(number)
+        
     # Finding the square root of the user input
     # and then stop at iterating once I find the that
     # square root because, I would have find all nonprimes.
@@ -22,22 +25,35 @@ while True:
         
             # Value is each number in my list 
             # based on the index position.
-            num_value = primes[num_idx]
-
+            num_value = numbers[num_idx]
+            print(num_value)
+            
             if num_value != 0:
                 
                 # I am finding all even numbers 
                 # and then assign them a zero.
                 start_idx = num_idx + num_value
+                assert start_idx >= 2
                 
-                for idx in range(start_idx, len(primes), num_value):
-                    primes[idx] = 0
-                         
-    for value in primes:
+                for idx in range(start_idx, len(numbers), num_value):
+                    numbers[idx] = 0
+            
+                    
+    # iterating through the primes list.                    
+    for value in numbers:
         
+        # filtering all values that i assigned to zero
+        # since they are not primes.
         if value != 0:
-            primes_list.append(value)
-    print(primes_list)
+            
+            # Adding all prime numbers in a new list.
+            assert type(primes) == list
+            primes.append(value)
+            
+    # print the new list that contains all prime numbers.       
+    print(f"This program will find all the prime numbers at or below N. Select that N: {user_input}")
+    
+    print(f"The prime numbers at or below {user_input} are {primes}.")
          
         
         
