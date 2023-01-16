6eff77785fdb0ac372cb07766ac2a870b451c5ec
martin <91570703+Marvian-programmer@users.noreply.github.com>
2023-01-16 10:33:30 -0700

    Added Angle class with some methods and created an instance for the Angle class.

--- Java/interfaces/com/Nkala_Interface/Main.java
+++ Java/interfaces/com/Nkala_Interface/Main.java
@@ -2,10 +2,16 @@ package com.Nkala_Interface;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
+//        Car car = new Car();
+//
+//        car.start_engine();
+//        car.drive();
+//        System.out.println(car);
 
-        car.start_engine();
-        car.drive();
-        System.out.println(car);
+        int num1 = 10;
+        int num2 = 20;
+
+        int result = (num1 < num2 ? num1-num2: num1 + num2); // Ternary operator.
+        System.out.println(result);
     }
 }
