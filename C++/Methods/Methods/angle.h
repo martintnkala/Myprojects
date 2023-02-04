03f3b90dd2b80ba493e9bd580115caabb65d2ea1
martin <91570703+Marvian-programmer@users.noreply.github.com>
2023-02-04 15:42:53 -0700

    add

--- C++/Methods/Methods/angle.h
+++ C++/Methods/Methods/angle.h
@@ -2,5 +2,59 @@
 #pragma once
 #include <stdio.h>
 #include <iostream>
-using namespace std;
 
+/*****************************
+ * Header File:
+ *    Angle.h
+ * Author:
+ *    Caleb Nuttal, Martin Nkala
+ * Summary:
+ *    This class validates and convert angles.
+ *****************************/
+
+/*************************
+ * ANGLE
+ * A single Angle.
+ *************************/
+class Angle
+{
+   
+private:
+   // This attribute will contain the radian value.
+   double radians;
+
+public:
+   
+   // Default Constructor.
+   Angle(): radians(0.0){};
+   
+   // Non-default Constructor.
+   Angle(double degrees);
+   
+   // Copy Constructor.
+   Angle(const Angle &rhs)
+   {
+      radians = rhs.radians;
+   }
+   
+   // These are setter methods for degrees and radians.
+   void setDegrees(double degrees);
+   void setRadians(double radians);
+   
+   // These are getter methods for degrees and radians.
+   double getDegrees() const;
+   
+   double getRadians() const;
+   
+   // Displays the value in degrees, to 1 decimal place
+   //of accuracy.
+   void display() const;
+   
+private:
+   
+   double convertToDegrees(double radians) const;
+   
+   double convertToRadians(double degrees) const;
+   
+   void normalize();
+};
