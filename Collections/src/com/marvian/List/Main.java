965d8eeba81f42a7c8ec6049a408101e393f8a39
Marvian-programmer <male.c201090@gmail.com>
2024-09-15 22:21:38 -0600

    Added code on collections interface

---
+++ Collections/src/com/marvian/List/Main.java
@@ -0,0 +1,26 @@
+package com.marvian.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> nums = new ArrayList<>(List.of(2, 6, 11, 10, 3));
+        Object[] numArray = nums.toArray();
+        System.out.println("Array iteration");
+        for (Object o : numArray) {
+            System.out.println(o + " ");
+        }
+        nums.sort(Integer::compare);
+
+        System.out.println("Using Stream API");
+        nums.stream()
+                .map(num -> num * 2)
+                .filter(num -> num % 3 == 1)
+                .forEach(System.out::println);
+
+
+        System.out.println(nums);
+    }
+}
